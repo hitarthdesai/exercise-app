e4315bdad3d5be27352c820320c0753d16abcefe
HittuDesai <hitarth.desai@mail.utoronto.ca>
2022-09-07 17:10:00 +0530

    Initial Commit

---
+++ exercise app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  exercise app
+//
+//  Created by Hitarth Jainul Desai on 2022-09-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ exercise app/exercise_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  exercise_appApp.swift
+//  exercise app
+//
+//  Created by Hitarth Jainul Desai on 2022-09-07.
+//
+
+import SwiftUI
+
+@main
+struct exercise_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
